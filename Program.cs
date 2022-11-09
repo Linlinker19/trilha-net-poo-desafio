a9fcc5032c8627894cb701fd7cd6e4cbe6af77f2
Linlinker19 <linlinker19@gmail.com>
2022-11-09 16:07:19 -0400

    Desafio Concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone N1 = new Nokia("(92) 99191-9292", "3225", "321564897516897", 16); 
+Smartphone Iphone12 = new Iphone(); 
+
+Iphone12.Numero = "(1) 3215-2121";
